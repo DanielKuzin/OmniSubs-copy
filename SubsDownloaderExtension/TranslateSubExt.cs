f0580c89b0a25bf4f291fe363f483c7a37f284f7
Daniel Kuzin <43601854+DanielKuzin@users.noreply.github.com>
2026-03-01 21:01:09 +0200

    Added hebrew translate  language

--- SubsDownloaderExtension/TranslateSubExt.cs
+++ SubsDownloaderExtension/TranslateSubExt.cs
@@ -24,6 +24,7 @@ namespace SubsDownloaderExtension
         private bool _isTranslateAndDownload = false;
         private string[] _languages = {
             "English",
+            "Hebrew",
             "Chinese",
             "Spanish",
             "Arabic",
